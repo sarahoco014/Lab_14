c05d09411d7c718d19bf324e4d4bbb63e2c1eebd
sarahoco014 <soconnor2@outlook.com>
2023-08-09 16:40:50 +0100

    adds application

---
+++ Airline_API/src/main/java/com/bnta/demo/AirlineApiApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineApiApplication.class, args);
+	}
+
+}
