863c1f12ba45aabf1ce53468550deb3355811c4c
sarahoco014 <soconnor2@outlook.com>
2023-08-09 17:01:05 +0100

    adds model code

--- Airline_API/src/main/java/com/bnta/demo/models/Flight.java
+++ Airline_API/src/main/java/com/bnta/demo/models/Flight.java
@@ -1,4 +1,94 @@
 package com.bnta.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "flights")
 public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String destination;
+
+    @Column
+    private int capacity;
+
+    @Column
+    private String departureDate;
+
+    @Column
+    private String departureTime;
+
+    @JsonIgnoreProperties({"flights"})
+    @ManyToMany(mappedBy = "flights") // has to match line 30:chocolate estates property name
+    private List<Passenger> passengers;
+
+    public Flight(String destination, int capacity, String departureDate, String departureTime) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.passengers = new ArrayList<Passenger>();
+    }
+
+    // default constructor
+    public Flight() {}
+
+    // getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(List<Passenger> passengers) {
+        this.passengers = passengers;
+    }
 }
--- Airline_API/src/main/java/com/bnta/demo/models/Passenger.java
+++ Airline_API/src/main/java/com/bnta/demo/models/Passenger.java
@@ -1,4 +1,76 @@
 package com.bnta.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "passengers")
 public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String emailAddress;
+
+
+    @ManyToMany
+    @JsonIgnoreProperties({"passengers"})
+    @JoinTable(
+            name = "pasengers_flights",
+            joinColumns = @JoinColumn(name = "passenger_id"),
+            inverseJoinColumns = @JoinColumn(name = "flight_id")
+    )
+    private List<Flight> flights;
+
+    public Passenger(String name, String emailAddress) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.flights = new ArrayList<>();
+    }
+
+    // default constructor
+    public Passenger(){}
+
+    // getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
 }
